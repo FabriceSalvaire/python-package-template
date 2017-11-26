ee2f23dfccecb23341181e02a9bda08f4517194e
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-11-26 23:16:16 +0100

    fixed copyright

--- setup_data.py
+++ setup_data.py
@@ -1,7 +1,7 @@
 ####################################################################################################
 #
 # @package_name@ - @licence_header_description@
-# Copyright (C) @year@ Salvaire Fabrice
+# Copyright (C) @year@ Fabrice Salvaire
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
